286229f49e810cff28da95448c45ac7ac97a03ed
tbm5k <tedburg5@gmail.com>
2021-12-23 23:29:11 +0300

    initial

---
+++ src/test/java/com/comforttransfers/comforttransfers/ComfortTransfersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comforttransfers.comforttransfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComfortTransfersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
