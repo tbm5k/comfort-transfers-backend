82e6933a449b6f5f3e6fea83e87b8ade92005d91
tbm5k <tedburg5@gmail.com>
2021-12-27 10:17:26 +0300

    mail sender

--- src/main/java/com/comforttransfers/comforttransfers/MailService.java
+++ src/main/java/com/comforttransfers/comforttransfers/MailService.java
@@ -1,10 +1,23 @@
 package com.comforttransfers.comforttransfers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     public Email sendEmail(Email email) {
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo("comforttransfers001@gmail.com");
+        mailMessage.setFrom(email.getFrom());
+        mailMessage.setText(email.getMessage());
+
+        mailSender.send(mailMessage);
         return email;
     }
 }
